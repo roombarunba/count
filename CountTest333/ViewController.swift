e849a96a16fb586970d8a05b31baf60222a7a921
nttr <nttr@BI-23560.local>
2017-08-02 14:27:48 +0900

    ＋、ー、clear機能追加

--- CountTest333/ViewController.swift
+++ CountTest333/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var label:UILabel!;
+    
+    var count: Int = 0;
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +23,21 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func plus(){
+        count += 1;
+        label.text = String(count);
+    }
+    
+    @IBAction func mai(){
+        count -= 1;
+        label.text = String(count);
+    }
+    
+    @IBAction func clear(){
+        count = 0;
+        label.text = String(count);
     }
 
 
